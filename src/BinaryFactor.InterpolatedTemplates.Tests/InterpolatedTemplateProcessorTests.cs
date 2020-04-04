ab2d82f58c82b74c6f9fa09b190fcb98966f5e0b
Bruno Alfirević <bruno.alfirevic@gmail.com>
2020-04-04 19:50:47 +0200

    Add tests.

--- src/BinaryFactor.InterpolatedTemplates.Tests/InterpolatedTemplateProcessorTests.cs
+++ src/BinaryFactor.InterpolatedTemplates.Tests/InterpolatedTemplateProcessorTests.cs
@@ -1,6 +1,31 @@
-﻿namespace BinaryFactor.InterpolatedTemplates.Tests
+﻿using Shouldly;
+using System;
+
+namespace BinaryFactor.InterpolatedTemplates.Tests
 {
     public class InterpolatedTemplateProcessorTests
     {
+        public void TestVanillaTemplates()
+        {
+            var lines = new FormattableString[]
+            {
+                $"Write('one');",
+                $"Write('two');",
+            };
+
+            FormattableString program = $@"
+                procedure PrintNumbers()
+                begin
+                    {lines}
+                end;";
+
+            var rendered = new InterpolatedTemplateProcessor().Render(program);
+
+            rendered.ShouldBe(@"procedure PrintNumbers()
+begin
+    Write('one');
+    Write('two');
+end;");
+        }
     }
 }
